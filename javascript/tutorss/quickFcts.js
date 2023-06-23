9a1a4e4dddbff70a89c2ad3b5cf6a7550dd38cfd
Atila Lima <atilalimade@gmail.com>
2023-06-23 12:36:57 -0300

    about Anon. and arrow functions

---
+++ javascript/tutorss/quickFcts.js
@@ -0,0 +1,15 @@
+//a function attributed to a var(making it the var reference)
+var anAnonimous = function(){
+    return 0
+}
+
+//an Arrow function: 'var' 'varName' = (args) => {return} 'value'.
+var anArrow = () => "THIS"
+
+//can do commands with it
+var anArrow = (a) => console.log("wow " + a)
+
+var integratingIt = () => {
+    anArrow('atila')
+    console.log(1+1)
+}//a better way to write functions quickly
