dfb62bb32704a989e7754c0d7381c5ece6400afd
Atila Lima <atilalimade@gmail.com>
2023-07-22 11:34:06 -0300

    method to sort (considering ponctuation)

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -1,1 +1,9 @@
 myList = ['ok', 'gabu', 'átila', 'miosina', 'actinina', 'íris', 'mão']
+console.log(myList.sort()) //not ideal, consider EN language.
+
+orderedInPort = myList.sort(
+    (a,b) => a.localeCompare(b)
+) //compare with the current language. acentuados vêm depois.
+
+console.log(orderedInPort) 
+
