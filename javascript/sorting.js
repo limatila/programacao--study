bb5ee67b53cadd435d2281702788907e79efa5e9
Atila Lima <atilalimade@gmail.com>
2023-07-24 13:28:39 -0300

    aplications of the sorting method

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -7,3 +7,22 @@ orderedInPort = myList.sort(
 
 console.log(orderedInPort) 
 
+inorderedInPort = myList.sort(
+    (a,b) => b.localeCompare(a)
+) //inverted
+
+console.log(inorderedInPort)
+
+
+const numbs = [1, 18, 17, 3, 90]
+console.log(numbs.sort(), "not ideal") //will bug out
+
+//specify the goal for better precision
+console.log(numbs.sort(
+    (a,b) => a-b
+))
+console.log(numbs.sort(
+    (a,b) => ((b**3)-a) //1 no final porq 1³ = 1, 1 < o resto
+)) 
+//sorting WILL change the original var. need to create a copy, or use a local scope to avoid changes. const won't prevent it.
+
