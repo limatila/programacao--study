5da73cd34eb11aa71a4ec3bc7d3be1258ba7f01f
Atila Lima <atilalimade@gmail.com>
2023-07-25 15:09:07 -0300

    A simple calculator

---
+++ javascript/tutorss/interactingWithHtml/Calculator/readme.md
@@ -0,0 +1,4 @@
+#OBS:
+A crude calculator made with js-html integration. only does the 4 basic operations. Has 2 places of input and all the results will be shown in the place of the green '0' number within the results area. The operations are also displayed in the console. also has a 'reset()' function that'll reset all the vars.
+
+maybe i'll develop more advanced calculation in the future(probably near)
--- javascript/tutorss/interactingWithHtml/Calculator/simpleCalculator.js
+++ javascript/tutorss/interactingWithHtml/Calculator/simpleCalculator.js
@@ -1,9 +1,47 @@
-//develop methods to make a calculator with an ui
+//how to implement functions with a function?
 
-num1 = document.getElementById("num1Textarea")
-num2 = document.getElementById("num2Textarea")
+function calculate(method){
+    var num1 = document.getElementById("num1Input").value
+    var num2 = document.getElementById("num2Input").value
+    num1 = parseInt(num1); num2 = parseInt(num2) //elements in html will ALWAYS be treated as string
 
-const add
-const subtract
-const multiply
-const divide
+    var total = 0
+
+    switch(method){
+        case 'add':{ //needs fix: adding is handling as a string
+            total = num1 + num2
+            console.log("added: " + total)
+            break;
+        }
+        case 'subtract':{
+            total = num1-num2
+            console.log("subtracted: " + total)
+            break;
+        }
+        case 'multiply':{
+            total = num1*num2
+            console.log("multiplied: " + total)
+            break;
+        }
+        case 'divide':{
+            total = num1/num2
+            console.log("divided: " + total)
+            break;
+        }
+        default:{console.log("select a valid calculation"); //TODO: const pow 
+                return "Not possible. Check console.";
+                break}
+    }
+    result = document.getElementById("calcResult") 
+    result.innerText = total
+}
+
+//how to use a seperated function to modify the p?
+
+const reset = () =>{
+    document.getElementById("calcResult").textContent = 0
+    document.getElementById("num1Input").value = null
+    document.getElementById("num2Input").value = null
+
+    console.log("RESETED.")
+}
