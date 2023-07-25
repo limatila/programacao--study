7d1814267b8ce687732055ab5dc3095e68cc6d38
Atila Lima <atilalimade@gmail.com>
2023-07-25 12:58:41 -0300

    preparing for a new page

---
+++ javascript/tutorss/interactingWithHtml/simpleCalculator.js
@@ -0,0 +1,9 @@
+//develop methods to make a calculator with an ui
+
+num1 = document.getElementById("num1Textarea")
+num2 = document.getElementById("num2Textarea")
+
+const add
+const subtract
+const multiply
+const divide
