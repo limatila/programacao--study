a07bc2d53234b520317fa7591064b7090f2e1bad
Atila Lima <95644070+limatila@users.noreply.github.com>
2023-08-09 12:44:12 -0300

    forgot how to use java

--- java/first.java
+++ java/first.java
@@ -4,5 +4,13 @@ class first{
         int a = 1;
         System.out.println(a);
 
+        float b = 2.1;
+
+        const myIterable = [1, 2.1]; //error
+
+        for(i = 0; i<myIterable.length; i++){
+            System.out.println("this variable: " + myIterable[i]);
+        };
+
     }
 }
