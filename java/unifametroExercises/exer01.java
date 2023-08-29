ba0fc01ba5e21f3e4030ede2bd419a07cb91b173
Atila Lima <atilalimade@gmail.com>
2023-08-29 20:56:23 -0300

    did until the 15º exercise

--- java/unifametroExercises/exer01.java
+++ java/unifametroExercises/exer01.java
@@ -1,4 +1,4 @@
-class Main{
+class exer01{
     public static void main(String[] args){
 
         //2.
@@ -21,7 +21,7 @@ class Main{
         };
 
         media = soma/myNums.length;
-        System.out.println(media);
+        System.out.println("A média é: "+ media);
             
         //5 ou 6.
         System.out.println("numero 1: "+ myNums[0] + "; numero 2: "+ myNums[1] +";");
@@ -36,12 +36,49 @@ class Main{
         System.out.println("nome: "+nome+ "; email: "+email+ "; telefone: " + telefone+";");
 
         //9.
-        int num1 = 1; int num2 = 10
-        System.out.println("Soma: " + (num1 = num2));
+        int num1 = 1; int num2 = 10;
+        System.out.println("Soma: " + (num1 + num2));
+
+        //11.
+        System.out.println("the third rounded part of num2 is " + num2/3);
 
         //10.
-        
+        System.out.println("multiplying num1 * num2" + num1 * num2);
 
-        
+        //12.
+        double nota1 = 8.6;
+        double nota2 = 5.8;
+        double media1 = (nota1+nota2)/2;
+        System.out.println("Nova Média" + media1);
+
+        //13.
+        int divisor = 10;
+        int dividendo = 73;
+        double quociente = dividendo/divisor;
+        int resto = dividendo%divisor;
+
+        System.out.println("------------");
+                System.out.println("divisor: "+ divisor + "\ndividendo: " + dividendo + "\nquociente: " + quociente + "\nresto: " + resto);
+        System.out.println("------------");
+
+        //14.
+        double nota3 = 3.8;
+        double nota4 = 9.0;
+        double mediaPonderada = (nota1 + (nota2*2) + (nota3*3) + (nota4*4))/10;
+        System.out.println("A Média Ponderada é: " + mediaPonderada);
+
+        //15.
+        float angulo = 30;
+        double seno = Math.sin(angulo);
+        double cosSeno = Math.cos(angulo);
+        double tangente = Math.tan(angulo);
+        double secante = 1/cosSeno;
+        double coSecante = 1/seno;
+        double coTangente = cosSeno/seno;
+
+        System.out.println("--------------");
+            System.out.println("Meu ângulo: " + angulo + "\nSeno dele: " + seno + "\nCosseno dele: " + cosSeno + "\nTangente dele: " + tangente + "\nSecante: " + secante
+            + "\nCossecante: " + coSecante + "\nCotangente: " + coTangente);
+        System.out.println("--------------");
     }
 }
