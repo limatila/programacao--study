859d57391e4d0d21a3b4e613846826473ab18fa1
Atila Lima <atilalimade@gmail.com>
2023-09-12 08:22:10 -0300

    finished fibonacci sequence

--- java/fibonacci.java
+++ java/fibonacci.java
@@ -1,15 +1,19 @@
-class Program{
-    public static void main(String[] args){
-    
-    int fib[] = {1, 1};
-    
-    for (var i = 0; i<10; i++) {
-        int x = fib[i] + fib[i+1];
-        fib.append(x); //will not append? find the right method.
+import java.util.Arrays;
 
-    };
-    
-    System.out.println(fib);
+class fibonacci {
+    public static void main(String[] args) {
+
+        int fib[] = { 1, 1 };
+
+        for (var i = 0; i < 10; i++) {
+            int newV = fib[i] + fib[i + 1];
+
+            fib = Arrays.copyOf(fib, fib.length + 1); // ceate copy, resize slots
+            fib[fib.length - 1] = newV;               // assign the new value to the last slot
+
+        };
+
+        System.out.println(Arrays.toString(fib));
 
     }
 }
