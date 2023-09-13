0649fd24892c36c44154597d49a4fe5b5e2fb17d
Átila Lima <atilalimade@gmail.com>
2023-09-12 22:13:57 -0300

    added link saving in a list

--- javascript/tutorss/navExtensions/markupApp/markup.app.js
+++ javascript/tutorss/navExtensions/markupApp/markup.app.js
@@ -1,11 +1,29 @@
 //DOMs
-const textInput = document.getElementById("inp1")
-const saveInputButton = document.getElementById("saveInpButton")
-const saveTabButton = document.getElementById("saveTabButton")
+textInput = document.getElementById("inp1")
+saveInputButton = document.getElementById("saveInpButton")
+saveTabButton = document.getElementById("saveTabButton")
+linkShow = document.getElementById("linkShowEl")
 
+websiteLinks = [];
 //functions
 const saveInput = () =>{
+    if(textInput.value.length === 0){
+        alert("Empty Input!");
+        throw console.error("Empty Input! function not executed.")
+    }
 
+    websiteLinks.push(textInput.value)
+
+    //TODO: show it.
+    totalContent = ""
+    for(i = 0; i < websiteLinks.length; i++){
+        totalContent += websiteLinks[i] + "\n"
+    }
+
+    linkShow.innerText = totalContent //user innerText for escaping literals.
+    linkShow.hidden = false
+
+    textInput.value = null //clear
 };
 
 const saveTab = () =>{
