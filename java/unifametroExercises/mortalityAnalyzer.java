3b1525fd85845f869003c3afadf2cc5789f164cc
Atila/Unifametro <atilalimade@gmail.com>
2023-11-14 09:28:27 -0300

    Completo. Completado o cálculo da porcentagem Adicionado contador de 24 meses

--- java/unifametroExercises/mortalityAnalyzer.java
+++ java/unifametroExercises/mortalityAnalyzer.java
@@ -8,39 +8,39 @@ public class mortalityAnalyzer {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Quantas crianças analisadas no periodo\t=> ");
-        int periodQuantity = scanner.nextInt(), deadQuantity = 0,
-                deadMasculine = 0, deadFeminine = 0;
-        String sexo = "";
+        System.out.print("Quantas crianças nascidas no periodo amostral\t=> ");
+        double periodQuantity = scanner.nextInt(), deadMasculine = 0, deadFeminine = 0;
+        int totalGreaterThen_twoY = 0, currentMonthLived;
+        String sex = "";
 
-        while (sexo != "vazio") {
+        while (sex != "vazio") {
             // asking which sex
             System.out.print("Qual o sexo da criança morta\t=> ");
-            sexo = scanner.next().toLowerCase();
-            if (sexo == "vazio") {
+            sex = scanner.next().toLowerCase();
+            if (sex.contains("vazio")) {
                 System.out.println("Stopping insertions...");
                 break;
             }
 
             System.out.print("Quantos meses a criança viveu\t=> ");
-            int numMonthsLived = scanner.nextInt();
+            currentMonthLived = scanner.nextInt();
+            if(currentMonthLived > 24){ totalGreaterThen_twoY++; };
 
-            if (sexo.charAt(0) == 'm') {
+            if (sex.charAt(0) == 'm') {
                 deadMasculine++;
-                deadQuantity++; // counting total of deaths
-            } else if (sexo.charAt(0) == 'f') {
+            } else if (sex.charAt(0) == 'f') {
                 deadFeminine++;
-                deadQuantity++; // counting total of deaths
             } else {
                 System.out.println("Sexo mal inserido, morte não contada.");
             }
         }
 
-        double mascDeadPorcentage = (deadMasculine / deadQuantity);
-        double femDeadPorcentage = (deadFeminine / deadQuantity);
+        double mascDeadPorcentage = (deadMasculine / periodQuantity)*100.0;
+        double femDeadPorcentage = (deadFeminine / periodQuantity)*100.0;
 
         System.out.println("A porcentagem de crianças meninos mortas foi " + mascDeadPorcentage + "%");
         System.out.println("A porcentagem de crianças meninas mortas foi " + femDeadPorcentage + "%");
+        System.out.println("O total de crianças com mais de 24 meses que morreram foi " + totalGreaterThen_twoY);
 
     }
 }
