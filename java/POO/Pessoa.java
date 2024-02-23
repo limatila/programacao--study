8e129cff086e3a91eb4de180f2f41faecfead2dc
AtilaLima <atilalimade@gmail.com>
2024-02-23 13:34:20 -0300

    Terminada a classe Pessoa concluidos os erros e implementado mais métodos. testados em aula2.java algumas formatações menores faltando...

--- java/POO/Pessoa.java
+++ java/POO/Pessoa.java
@@ -3,20 +3,28 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 class Pessoa {
+    //atributos -- geralmente privados, acessados sempre por um método!
     private String Nome = "";
     private int idade = 0;
     private char sexo = 'N';
     private String dataAniversario = "";
+    private String anoNascimento = "";
     private String natalidade = "";
     private String CPF = "0"; 
 
-    //setters
-    public void mudarNome(String nomeInserido){
+
+    //setters -- melhor usar um constructor
+    public void mudarNome(String nomeInserido){ //precisa especificar que tipo é o Arg recebido.
         this.Nome = nomeInserido;
     }
-    public void mudarDataAniversario(String anivInserido){
-        this.dataAniversario = anivInserido;
+    public void mudarDataAniversario(String anivInserido){ //inserir 'ddmmyyyy'
+        String anoAniv = anivInserido.substring(4);
+        String dia_mesAniv = anivInserido.substring(0,4);
+        
+        this.dataAniversario = dia_mesAniv;
+        this.anoNascimento = anoAniv;
     }
+    
     public void mudarNatalidade(String cidadeInserida){
         this.natalidade = cidadeInserida;
     }
@@ -29,24 +37,34 @@ class Pessoa {
 
 
     //funcionais
-    public boolean fazAniversario(){
+    public String descreverSexo(){
+        return sexo == 'M' ? "Homem" : sexo == 'F' ? "Mulher" : "NULL";
+    }
+
+    public void resumir(){
+        System.out.println("A pessoa referida tem nome " + this.Nome + ", tem "+ this.idade 
+        + " anos de idade, e é " + descreverSexo() + ". Nasceu em " + this.natalidade
+        + ". Sua data de nascimento é " + (this.dataAniversario.concat(this.anoNascimento + "."))); //! formatar melhor!
+    }
+
+    public boolean fazAniversario(){ //pode ser usado pra vários propósitos: polimorfismo.
         String dataGerada = new SimpleDateFormat("ddMM").format(Calendar.getInstance().getTime());
-        return dataGerada == dataAniversario;
+        return dataGerada.equals(dataAniversario);
     }
     
     public void aumentarIdade(int maisIdade){
         idade += maisIdade;
     }
 
-    public void parabenizar(){
-        if(fazAniversario() == true){
+    public void parabenizar(){  //abstração, uma camada maior de lógica que possui um objetivo final definido.
+        if(this.fazAniversario() == true){
             aumentarIdade(1);
             System.out.println("Parabens " + this.Nome + "!!!");
         } else {
             String dataGerada2 = new SimpleDateFormat("ddMM").format(Calendar.getInstance().getTime()).substring(2, 4);
             String dataAnivReduzida = this.dataAniversario.substring(2, 4);
             int mesesFaltando = (Integer.parseInt(dataGerada2) - Integer.parseInt(dataAnivReduzida));
-            System.out.println("Ainda faltam " + mesesFaltando + " meses para o seu aniversário, " + this.Nome + ".");
+            System.out.println("Ainda faltam mais ou menos " + mesesFaltando + " meses para o seu aniversário, " + this.Nome + "."); //!não leva em conta os dias
         }
     }
     
--- java/POO/aula2.java
+++ java/POO/aula2.java
@@ -5,7 +5,7 @@ public class aula2 {
     public static void main(String[] args) {
         Pessoa pess1 = new Pessoa();
         pess1.mudarNome("Átila");
-        pess1.mudarDataAniversario("1602");
+        pess1.mudarDataAniversario("16012005");
         pess1.mudarSexo("M");
         pess1.mudarCPF("12345678999");
         pess1.mudarNatalidade("Fortaleza");
@@ -17,8 +17,21 @@ public class aula2 {
         Pessoa pess2 = new Pessoa();
         pess2.mudarNome("Carlos");
         pess2.aumentarIdade(24);
-        pess2.mudarDataAniversario("2202");
+        pess2.mudarDataAniversario("23022005"); //teste com a data atual!
 
         pess2.parabenizar();
+
+        
+        Pessoa pess_ultimo = new Pessoa();
+        pess_ultimo.mudarNome("Nana");
+        pess_ultimo.mudarDataAniversario("30122002");
+        pess_ultimo.mudarNatalidade("Fortaleza");
+        pess_ultimo.mudarSexo("F");
+        pess_ultimo.aumentarIdade(20);
+
+
+        System.out.println("");
+        pess1.resumir();
+        pess_ultimo.resumir();
     }
 }
