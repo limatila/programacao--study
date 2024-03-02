6c7b180272bb95c746fca5b47c36a1144e55dc8b
Atila/Unifametro <atilalimade@gmail.com>
2024-03-01 22:56:41 -0300

    Consertando lugar de classe local adicionado 2 alunos para uso

--- java/POO/aula3.java
+++ java/POO/aula3.java
@@ -1,29 +1,29 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class aula3 {
-    class aluno{
-        private String nome;
-        private int idade;
-        private String curso;
-        private int anoIngresso;
-        private int qtdDisciplinasAprov;
+class Aluno{
+    private String nome;
+    private int idade;
+    private String curso;
+    private int anoIngresso;
+    private int qtdDisciplinasAprov;
 
-        public aluno(String nome, int idade, String curso, int anoIngresso, int qtdDisciplinasAprov){
-            this.nome = nome;
-            this.idade = idade;
-            this.curso = curso;
-            this.anoIngresso = anoIngresso;
-            this.qtdDisciplinasAprov = qtdDisciplinasAprov;
-        }
-
-        public void tempoIngressado(){
-            int anoAtual = Integer.parseInt(new SimpleDateFormat("YYYY").format(Calendar.getInstance().getTime()));
-            int tempoTotal = anoAtual - this.anoIngresso;
-            System.out.println(tempoTotal);
-        }
-
+    public Aluno(String nome, int idade, String curso, int anoIngresso, int qtdDisciplinasAprov){
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+        this.anoIngresso = anoIngresso;
+        this.qtdDisciplinasAprov = qtdDisciplinasAprov;
     }
+
+    public void tempoIngressado(){
+        int anoAtual = Integer.parseInt(new SimpleDateFormat("YYYY").format(Calendar.getInstance().getTime()));
+        int tempoTotal = anoAtual - this.anoIngresso;
+        System.out.println(tempoTotal);
+    }
+}
+
+public class aula3 {
     public static void main(String[] args) throws Exception {
         Animal Dog = new Animal("Loki", 6, "mamifero", 6.1);
         System.out.println(Dog.calcularConsumoAlimento());
@@ -42,5 +42,9 @@ public class aula3 {
         System.out.println(Humano.calcularConsumoAlimento());
 
         System.out.println("\n\nAlunos-------------------");
+        Aluno aluno1 = new Aluno("Matheus", 19, "medicina", 2022, 16);
+        Aluno aluno2 = new Aluno("Alceu", 51, "SMD", 2023, 5);
+
+        
     }
 }
