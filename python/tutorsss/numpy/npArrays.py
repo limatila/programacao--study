dcb644f0e1fc372a513b12732782f8c4ee5aaae4
Atila Lima <atilalimade@gmail.com>
2024-07-18 14:21:53 -0300

    Adicionando destaque de tópicos

--- python/tutorsss/numpy/npArrays.py
+++ python/tutorsss/numpy/npArrays.py
@@ -10,7 +10,7 @@ import numpy as np #convention
 array1 = np.array([1, 3, 5, 10])
 arrayRange_10 = np.arange(0, 10, 2) #mounts the array in a range, and can take steps (third Arg)
 
-#properties----
+#*Properties----
 print(array1.size) #length of array
 print(array1.ndim) #number of dimensions in array
 
@@ -20,7 +20,7 @@ print(array1.dtype) #prevalent type of data
 print(array1.itemsize) #size in bytes of prevalent type of data
 
 
-#operations----
+#*Operations----
 tspac(); line(23)
 list1 = array1.tolist() #a copy, for example.
 print(list1) 
@@ -47,7 +47,7 @@ calc1 = ( array2 / array1 ) #Broadcasting, it's selective calculation, can be do
 print(array2, calc1, sep=";\n") #This calc returns to the expoent used in 35
 
 
-#more operations
+#*more operations
 tspac(); line(47)
 print(np.sqrt( np.array([4, 9, 144]) ))
 
@@ -58,7 +58,7 @@ print(dotProduct)
 print(alsoDotProduct)
 
 
-#Bidimensional
+#*Bidimensional
 tspac(); line(61)
 array3 = np.array( [[1, 2, 3], 
                     [2, 10, 25]] ) #i and j accordingly. accepts a list containing lists
@@ -72,7 +72,7 @@ print(array3[0:, 1:])#requires all the dimensions
 print("\nInverting:\n", np.flip(array3[0:, 1:]))
 
 tspac(); line(73)
-#Size Operations:
+#*Size Operations:
 array4 = np.array( [[10, 103, 59, 88, 23],
                    [5, 30, {"a": 59}, 22, 500]] )
 
@@ -93,7 +93,7 @@ array3 = array3.flatten()
 print("Rearranging every element for a row:\n", array3[:, np.newaxis])
 
 print(""); line(91)
-#Boolean Reshaping: reshapes according to a set rule
+#*Boolean Reshaping: reshapes according to a set rule
 #comparators and logic operations will be performed in every item and returned as true/false
 def evenArray(arr):
     return ( (arr % 2) == 0 ) #can be done in any sentence.
@@ -118,7 +118,7 @@ resultedSumArray = initialArray + reshapedArray #every single value + every sing
 print("Broadcasting sum to all of the elements:\n", resultedSumArray)
 
 tspac(); line(121)
-#Functions and Axis
+#*Functions and Axis: products and calculations built-in
 print("Sum of the resulted array:\n", resultedSumArray.sum()) #of all the elements
 print("Sum of the array in the vertical way:\n", resultedSumArray.sum(axis=0)) #of all the from top to bottom in the columns imaginary grid
 print("Sum of the array in the horizontal way:\n", resultedSumArray.sum(axis=1)) #of all the from left to right in the rows imaginary grid
@@ -134,7 +134,7 @@ print("Smaller and Greater element: ", array3.max(), array3.min())
 #TODO: search for more methods and effects on these functions
 
 tspac(); line(137)
-#Data types
+#*Data types
 alwaysInt = np.array([3.0001, 25.0], dtype = np.int16)
 alwaysFloat = np.array([2, 1001], dtype = np.float32)
 
@@ -142,16 +142,16 @@ print("Data types of the arrays specified:")
 print(alwaysInt.dtype)
 print(alwaysFloat.dtype)
 
-#Copying
+#*Copying
 print(""); line(147)
 array7 = np.array([True, False, True])
 array8 = np.array(array7.copy()) #New reference created, won't be modified by the other.
 print(array7, "Is a new array")
 
-#Generating arrays: new arrays from produced data
+#*Generating arrays: new arrays from produced data
 zerosArray = np.zeros((5, 5)) #only 0s in the selected grid 
-
-
-
+onesArray = np.ones((5,5)) #only 1s
+selectedNumberArray = np.full((2,4), 100) #creates with the shape, with the desired value
+fillLikeOtherArray = np.full_like(selectedNumberArray, 5500) #creates with shape of another, selecting a value to fill
 
 
