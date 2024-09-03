a3b7a59ccdeb47ce5bc033c762d7633f39096340
Atila Lima <atilalimade@gmail.com>
2024-09-03 19:02:34 -0300

    Refatorando pasta de Pages

--- python/tutorsss/librs/django/projetos/Receitas/views.py
+++ python/tutorsss/librs/django/projetos/Receitas/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def HOME(request):
-    return render(request, "home-receitas.html",
+    return render(request, "pages/home-receitas.html",
                   context={},
                   content_type="text/html",)
