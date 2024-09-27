b39ae402280c716cf2105e606d092b05b8d1088d
Atila Lima <atilalimade@gmail.com>
2024-09-26 23:11:07 -0300

    implementando gerador de imagens para os cards ATENÇÂO: substituir logo por outro tipo de demonstração, conteúdo nsfw. faltando estilizar para cortar imagens, faltando estilizar para melhor arranjar a grid

--- python/tutorsss/librs/django/projetos/Receitas/views.py
+++ python/tutorsss/librs/django/projetos/Receitas/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from tests.fakeDataTests.Receitas.fakes import main as genFakeData
+
 # Create your views here.
 
 def HOME(request):
     return render(request, "pages/home-receitas.html",
-                  context={},
-                  content_type="text/html",)
+                  context={
+                      "receitas": [ genFakeData(num) for num in range(5) ] 
+                    },
+                  content_type="text/html")
