3641fcbcb5a2ba98ebe13be48f705fa199290528
Atila Lima <atilalimade@gmail.com>
2024-09-30 17:54:55 -0300

    config: iniciando configuração para site próprio de cada receita INCOMPLETO!

--- python/tutorsss/librs/django/projetos/Receitas/urls.py
+++ python/tutorsss/librs/django/projetos/Receitas/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HOME),
+    path('receita', views.RECEITA),
 ]
--- python/tutorsss/librs/django/projetos/Receitas/views.py
+++ python/tutorsss/librs/django/projetos/Receitas/views.py
@@ -7,6 +7,16 @@ from tests.fakeDataTests.Receitas.fakes import main as genFakeData
 def HOME(request):
     return render(request, "pages/home-receitas.html",
                   context={
-                      "receitas": [ genFakeData(num) for num in range(5) ] 
+                      "pageDetails": {"isHome": True},
+                      "receitas": [ genFakeData(num) for num in range(5) ],
+                    },
+                  content_type="text/html")
+
+def RECEITA(request, idReceita):
+    return render(request, "pages/receita.html/{{ idReceita }}",
+                  context={
+                      "receita": genFakeData(idReceita),
+                      "pageDetails": {"isReceita": True,
+                                      "isHome": False},
                     },
                   content_type="text/html")
