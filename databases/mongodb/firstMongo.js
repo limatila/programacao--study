95c33c272b4663cb0c9bd0e060b5a74e72f7cf05
Atila Lima <atilalimade@gmail.com>
2024-09-30 16:57:35 -0300

    Renomeando o first script

--- databases/mongodb/firstMongo.js
+++ databases/mongodb/firstMongo.js
@@ -1,7 +1,7 @@
 //First creation and insertion
 
 //! IMPORTANT: need to use it in MongoShell.
-// example in mongosh: load( "first.js" )
+// example in mongosh: load( "firstMongo.js" )
 
 //* Connecting to local mongo for shell.
 db = connect("mongodb://localhost/")
