840b3fb16fbbda85fd90c186dd963941fd26e5d5
Atila Lima <atilalimade@gmail.com>
2024-10-18 19:33:57 -0300

    new: view para abrir uma receita aleatória adicionando comentários de próximas ideias

--- python/tutorsss/librs/django/projetos/Receitas/views.py
+++ python/tutorsss/librs/django/projetos/Receitas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from tests.fakeDataTests.Receitas.fakes import main as genFakeData
+from random import randint
 
 # Create your views here.
 
@@ -11,6 +12,7 @@ def HOME(request):
                       "pageDetails": {"isMainMenu": True,
                                       "isDetailMenu": False},
                       "receitas": [ genFakeData(num) for num in range(5) ], #* gera 5 fakes pra uso
+                      #? receitas devem ser pegues do DB, categorizando por número de cliques
                     },
                   content_type="text/html")
 
@@ -18,8 +20,13 @@ def RECEITA(request, idReceita):
     return render(request, "pages/receita.html/",
                   context={
                       "title": f"Receita id{idReceita}",
-                      "receita": genFakeData(idReceita), #TODO: Ao implementar BD, idReceita deve ser usado para consultar ela.
                       "pageDetails": {"isMainMenu": False,
                                       "isDetailMenu": True},
+                      "receita": genFakeData(idReceita), 
+                      #? receita deve ser peque do DB, onde idReceita deve ser o ID PK 
                     },
                   content_type="text/html")
+
+def randomRECEITA(request): # Para botão 'Me mostre uma nova!'
+    randomID = randint(0, 5)
+    return RECEITA(request, randomID)
