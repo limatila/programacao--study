c3af43fbe64398d2ed7c687e6bf8f7086a6552aa
Atila Lima <atilalimade@gmail.com>
2024-10-31 21:20:12 -0300

    Aprendendo mais sobre Migrations criando models novos falta implementação correta de models.TextChoices, está deixando apenas 1 categoria por vez

--- python/tutorsss/librs/django/projetos/Receitas/admin.py
+++ python/tutorsss/librs/django/projetos/Receitas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
+from .utils import modelFormatters
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Receita)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
