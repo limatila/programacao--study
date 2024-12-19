a93421cce4ca6afaeadb3c637618a0b7ce787b19
Atila Lima <atilalimade@gmail.com>
2024-12-19 18:31:34 -0300

    Conf: adicionando redirecionamento à Home Agora Root (localhost:8000/) redireciona logo para o início da página

--- projetos/Receitas/urls.py
+++ projetos/Receitas/urls.py
@@ -1,7 +1,14 @@
 from . import views
 from django.urls import path
+from django.shortcuts import redirect
+
+#Definindo Redirecionamento da Root
+def ROOT(request):
+    return redirect('Home')
+
 
 urlpatterns = [
+    path('', ROOT, name="Root"),
     path('home/', views.HOME, name="Home"),
     path('receita/<int:idRequest>/', views.RECEITA, name="Receita"),
     path('categoria/<int:idRequest>/', views.CATEGORIA, name="Categoria"),
