53fcc6107782bbdb10ac4b56d91a174185b38e45
Atila/Celular <atilalimade@gmail.com>
2025-01-05 19:57:46 -0300

    Final: sobre definição de Packages

--- python/tutorsss/packageUsage/utilitaries/formattings.py
+++ python/tutorsss/packageUsage/utilitaries/formattings.py
@@ -1,2 +1,27 @@
-def formatString(string: str):
-	return string.stripe() + f", plus the last char: {name[-1]}"
+def formatString(string: str) -> str:
+	return string.strip() + f", \nplus the last char: {string[-1]}"
+	
+def formatNameAttr(object) -> str:
+	listNameAttrs: list[str] = ["name", "title"]
+	nameFound: str = ""
+	
+	for aName in listNameAttrs:
+		if hasattr(object, aName.lower()):
+			nameFound = aName.lower()
+			break;
+	
+	if nameFound == "":
+		return "the Attr was not found."
+	
+	return getattr(object, nameFound) + " is your name! Nice!"
+
+if __name__ == "__main__":
+	test = formatString("hey, how are you?")
+	print(test)
+	
+	class Person:
+		name = "Atila"
+	
+	aPerson = Person()
+	test2 = formatNameAttr(aPerson)
+	print("\n\n" + test2)
