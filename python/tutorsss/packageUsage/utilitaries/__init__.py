4d92195a8929a3984aeae970048c86e5538bd70e
Atila/Celular <atilalimade@gmail.com>
2025-01-05 19:57:46 -0300

    Final: sobre definição de Packages

--- python/tutorsss/packageUsage/utilitaries/__init__.py
+++ python/tutorsss/packageUsage/utilitaries/__init__.py
@@ -1,11 +1,13 @@
+#DO NOT RUN THIS FILE ALONE!
+
+from .calculations import equation_on_two_degree 
 
 #listagem de todos os modulos a serem importados com operador *
 __all__: list[str] = [
  "calculations", 
- "formattings"
+ "formattings",
+ "equation_on_two_degree", #para importar a função/variavel completa 
 ]
 
 if __name__ != "__main__": #fora do init
     print(f"utilitaries imported")
-else:									  #dentro do init
-	print("python running!")
