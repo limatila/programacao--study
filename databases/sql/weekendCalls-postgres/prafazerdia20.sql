287b2f169d64c974c510703da3bfa1c60ec46cc0
Atila Lima <atilalimade@gmail.com>
2025-01-11 18:08:34 -0300

    Call dia 11/01: Terminando Estrutura de Escola Terminada estrutura com base no diagrama da Call Renomeado arquivo propriamente

--- databases/sql/weekendCalls-postgres/prafazerdia20.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Professores (
-	PK_idProfessor SERIAL PRIMARY KEY,
-	nome VARCHAR NOT NULL,
-	CPF VARCHAR NOT NULL UNIQUE
-);
-
-ALTER TABLE Materias ADD CONSTRAINT (FK_idProfessor) REFERENCES Professores.PK_idProfessor;
