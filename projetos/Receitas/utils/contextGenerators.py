b658f8f901811707ba1432777fe08881a4cd5f10
Atila Lima <atilalimade@gmail.com>
2025-01-21 14:27:08 -0300

    primeira configuração de 404 dinâmico (dependendo da seção)

--- projetos/Receitas/utils/contextGenerators.py
+++ projetos/Receitas/utils/contextGenerators.py
@@ -1,10 +1,11 @@
-MAIN_BASE_CONTEXT = {"isDefaultMenu": False,
-                     "isSimpleMenu": False,
-                     "isReceitaDetail": False,
-                     "isCollectionMenu": False}
-
 #Geradores padronizados de 'Context's para Views.py
 
+MAIN_BASE_CONTEXT = {
+    "isDefaultMenu": False,
+    "isSimpleMenu": False,
+    "isReceitaDetail": False,
+    "isCollectionMenu": False,
+}
 
 def genMainContext(pageToContext: int) -> dict[str, bool]:
     generatedContext = dict(MAIN_BASE_CONTEXT)
@@ -31,3 +32,26 @@ def genMainContext(pageToContext: int) -> dict[str, bool]:
             )
 
     return generatedContext
+
+
+def genNotFoundContext(pageToContext: int) -> str:
+    ##Pra cada 404:
+    # 1: Home Menu -- receitas não encontradas
+    # 2: Receita Detailed -- receita não encontrada
+    # 3: Category Menu -- receitas não encontradas
+    # 4: Collection Menu -- coleções não encontradas
+    ## Diferenças: Menu para visualizar cards de receitas, Detail para visualizar amplicação do card.
+
+    match (pageToContext):
+        case 1:
+            return "não há receitas aqui"
+        case 2:
+            return "essa receita não existe"
+        case 3:
+            return "não há receitas aqui"
+        case 4:
+            return "não há coleções aqui"
+        case _:
+            raise Exception(
+                "Bad choice for genMainContext generator. Only allowed integers between 1 and 4."
+            )
--- projetos/Receitas/views.py
+++ projetos/Receitas/views.py
@@ -5,34 +5,41 @@ from .models import Receita
 #test: from tests.fakeDataTests.Receitas.fakes import main as genFakeData
 
 from random import randint
-from .utils.contextGenerators import genMainContext
+from .utils.contextGenerators import *
 from .utils.queryFormatters import topLikes
 
 # View padrão code 404
-def NOT_FOUND(request):
-    return render(
-        request, "404.html",
-        status=404 )
+def NOT_FOUND(request, pageNumberForGen: int):
+    return render(request, "404.html", status=404,
+    context={"pageDetails": genMainContext(pageNumberForGen),
+             "404_message": genNotFoundContext(pageNumberForGen)} )
 
 # Create your views here.
 
 def HOME(request):
-    receitasQueried = topLikes(Receita.objects.all())[:12] #TODO: adicionar filtro por número de likes
-    contextGenerated = genMainContext(1)
-    contextGenerated.update({"isHome": True})
+    receitasQueried = topLikes(
+        Receita.objects.all())[:
+                               12]  #TODO: adicionar filtro por número de likes
 
-    return render(request, "pages/menu.html",
+    if not receitasQueried:
+        return 'NOT_FOUND'(request, 1)
+    else:
+        contextGenerated = genMainContext(1)
+        contextGenerated.update({"isHome": True})
+
+    return render(request,
+                  "pages/menu.html",
                   context={
                       "pageDetails": contextGenerated,
                       "receitas": receitasQueried
-                    },
+                  },
                   content_type="text/html")
 
 
-def RECEITA(request, idRequest):
+def RECEITA(request, idRequest: int):
     try: receitaQueried = Receita.objects.get(idPage=idRequest)
     except Receita.DoesNotExist:
-        return NOT_FOUND(request)
+        return NOT_FOUND(request, 2)
 
     return render(
         request, "pages/receita.html/",
@@ -90,6 +97,6 @@ def randomRECEITA(request):  #TODO: Colocar em botão 'Me mostre uma nova!'
     randomID = randint(0, maxReceitas)
 
     return (
-        RECEITA(request, 
+        RECEITA(request,
                 randomID)
     )
