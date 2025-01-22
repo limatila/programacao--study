09e18b360bf62ed4799d9441249397cb745b9454
Atila Lima <atilalimade@gmail.com>
2025-01-22 16:16:55 -0300

    Primeiro pytest criado, exemplificando alguns asserts

---
+++ projetos/tests/testCases/defaultTestCase_test.py
@@ -0,0 +1,18 @@
+from django.test import TestCase #Derivado do módulo unittest
+
+
+class DefaultTestTemplate(TestCase):
+    def test_default_unit_test(self):
+        self.assertTrue(True)
+        self.assertFalse(False)  # Falsy values
+        self.assertEqual(1 + 1, 2)
+        self.assertIsNone(None)
+        self.assertIsNotNone(False)  # Qualquer outro que None
+        self.assertIsInstance("hello", str)
+        self.assertIsInstance(3.14, float)
+        self.assertIsInstance((1, 2, 3), tuple)
+
+        #Comparativos
+        self.assertGreater(10, 5)
+        self.assertGreaterEqual(10, 10)  #e mais
+        self.assertLess(5, 10)
