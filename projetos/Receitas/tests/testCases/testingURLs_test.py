ef99b702a32db764a09c24b8cff6efa20d454a9b
Atila Lima <atilalimade@gmail.com>
2025-01-28 09:54:26 -0300

    Movendo pasta de testes para dentro de Receitas

--- projetos/Receitas/tests/testCases/testingURLs_test.py
+++ projetos/Receitas/tests/testCases/testingURLs_test.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from django.conf import settings
-
 class TestPageURLs(TestCase):
     def test_shallReturnCorrectRootUrl(self):
         url = reverse('Root')
