9a2523b718a98dbddf20962026e81da62dcd3527
Atila Lima <atilalimade@gmail.com>
2025-01-29 14:38:19 -0300

    modificando comando runserver

--- activate.ps1
+++ activate.ps1
@@ -4,4 +4,4 @@
 # Set-ExecutionPolicy Unrestricted -Scope Process
 # venv/scripts/Activate
 $host.ui.RawUI.WindowTitle = 'Debug (Django)'
-py .\projetos\manage.py runserver localhost:35001 --insecure
+py .\projetos\manage.py runserver localhost:35001
