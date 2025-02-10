2b50a51af5e1807ebfb6984d542d86fc6c3d5ceb
Átila Lima <atilalimade@gmail.com>
2025-02-10 11:16:39 -0300

    runserver: modificando para ativação rápida

--- activate.ps1
+++ activate.ps1
@@ -1,7 +1,7 @@
 # Use this in PowerShell
 
 # for Venv
-# Set-ExecutionPolicy Unrestricted -Scope Process
-# venv/scripts/Activate
+Set-ExecutionPolicy Unrestricted -Scope Process
+.venv/scripts/Activate
 $host.ui.RawUI.WindowTitle = 'Debug (Django)'
 py .\projetos\manage.py runserver localhost:35001
