fc54c10ce4464b69abd9f2e1f87143098b98d65d
Átila Lima <atilalimade@gmail.com>
2025-02-10 12:13:26 -0300

    test: adicionando test de redirect da Root > Home

--- projetos/Receitas/tests/testCases/Views_test.py
+++ projetos/Receitas/tests/testCases/Views_test.py
@@ -4,13 +4,22 @@ from django.urls import resolve, reverse
 from Receitas import views
 from Receitas.urls import ROOT
 
-class TestViewFunctions(TestCase):
+class TestViewAssignments(TestCase):
     #* Testes estáticos para assegurar nomes de URL e função de View
     def test_shallResolveCorrectRootView(self) -> None:
         view = resolve('/')  #Resolve a URL para a view correspondente
         
         self.assertIs(view.func, ROOT)
         print(f'Func {view.func.__name__} está designada.')
+
+    def test_shallRedirectRootToHome(self) -> None:
+        view = resolve('/')  #Resolve a URL para a view correspondente
+        
+        returnedUrl = view.func(request=None).url
+
+        self.assertIs(view.func, ROOT)
+        self.assertEqual( returnedUrl.strip(), reverse('Home').strip() ) #Verificando redirect
+        print(f'Func {view.func.__name__} redirecionou para url {returnedUrl}')
 
     def test_shallResolveCorrectHomeView(self) -> None:
 
