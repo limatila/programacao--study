b5adbbbf41b21fbf5c69c867de2cf5ab25126962
Átila <atilalimade@gmail.com>
2025-03-18 13:58:52 -0300

    docs: updating docstring for connection dependencys
    also refactoring code to have similar structure

--- pokedex/dependencies/connections.py
+++ pokedex/dependencies/connections.py
@@ -5,48 +5,45 @@ from .config import DB_ENGINE_CHOICE, sqlite_heading, pgsql_heading
 #* Engines: for manual usage
 def get_engine(engineChoice: str  = "sqlite3" ):
     """
-    Returns a engine for SQLModel Sessions.
-        Will Accept:
-        #SQLite:
-        - 'sqlite'
-        - 'sqlite3'
+    Returns a SQLModel Engine for SQLModel Sessions.
+    
+        engineChoice: select the DB to be used, mainly with config.DB_ENGINE_CHOICE. 
+            - 'sqlite'
+            - 'sqlite3'
+            - 'pgsql'
 
-        #PostgreSQL
-        - 'pgsql'
-
-        #Others:
-        - will return an Anon-Exception.
+        Raises Exception for invalid cases
     """
     match(engineChoice):
+        case "sqlite" | "sqlite3":
+            return create_engine(f"sqlite:///{sqlite_heading['filename']}")
         case "pgsql":
             return create_engine(f"postgresql://{pgsql_heading['user']}:{pgsql_heading['password']}" +
                                  f"@{pgsql_heading['adress']}/{pgsql_heading['db']}",
                                  echo=True, pool_pre_ping=True)
-        case "sqlite" | "sqlite3":
-            return create_engine(f"sqlite:///{sqlite_heading['filename']}")
         case _:
-            raise Exception("Not a valid choice of engine, please select between \"sqlite\" and \"pgsql\".")
+            raise Exception("Not a valid choice of engine, \nplease select between \"sqlite\" and \"pgsql\".")
 
 #* For FastAPI.Depends
 def get_db_session_dependency():
     """
-    Yields a created session for use in FastAPI decorated Functions\n
-    Expects that DB_ENGINE_CHOICE global variable is either:
-        #SQLite:
+    Yields a created session for use in FastAPI decorated Functions with the 'Depends' function in params\n
+    Expects that config.DB_ENGINE_CHOICE global variable is either:
         - 'sqlite'
         - 'sqlite3'
-
-        #PostgreSQL
         - 'pgsql'
+
+    Raises Exception for invalid cases
     """
-    if DB_ENGINE_CHOICE == "sqlite" or DB_ENGINE_CHOICE == "sqlite3":
-        with Session(get_engine("sqlite3")) as session:
-            yield session
-    elif DB_ENGINE_CHOICE == "pgsql":
-        with Session(get_engine("pgsql")) as session:
-            yield session
-    else:
-        raise Exception("Not a valid choice of engine for the generator, please select between \"sqlite\" and \"pgsql\".")
+    match(DB_ENGINE_CHOICE):
+        case "sqlite" | "sqlite3":
+            with Session(get_engine("sqlite3")) as session:
+                yield session
+        case "pgsql":
+            with Session(get_engine("pgsql")) as session:
+                yield session
+        case _:
+            raise Exception("Not a valid choice of engine for the generator, \nplease select between \"sqlite\" and \"pgsql\".")
     
 if __name__ == "__main__":
     an_engine = get_engine(DB_ENGINE_CHOICE)
