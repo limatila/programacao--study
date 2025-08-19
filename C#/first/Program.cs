d79c5230b7b0d49487906c893b8b4dec33b59211
Átila Lima <atilalimade@gmail.com>
2025-08-19 20:52:52 -0300

    first: creating a dog that barks
    obs: tested/runned with 'scriptcs'

---
+++ C#/first/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// namespace First.Animals
+// {
+//     public class Dog
+//     {
+//         public string Name { get; set; }
+//         public int Id { get; set; }
+//     }
+// }
+
+// Instantiating dog using its constructor
+Dog dog = new Dog("Felermino", 2);
+Dog dog2 = new Dog("Filipino", 7);
+
+// barking
+dog.Bark();
+dog2.Bark();
+
+public class Dog
+{
+    protected string Name { get; set; }
+    protected int Age { get; set; }
+    public Dog(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // methods
+    public void Bark()
+    {
+        if (Age < 5)
+        {
+            Console.WriteLine(Name + ": au");
+        }
+        else
+        {
+            Console.WriteLine(Name + ": AAAUUU");
+        }
+    }
+
+}
